05072fbc5a6739fb4116c8e64a5609cbd1a81fa5
Vinayak Patel <76094252+Vinayak-Patel@users.noreply.github.com>
2025-02-20 16:47:08 -0500

    updated losses file

--- losses.py
+++ losses.py
@@ -1,4 +1,5 @@
 import torch
+import pytorch3d
 
 # define losses
 def voxel_loss(voxel_src,voxel_tgt):
@@ -15,18 +16,13 @@ def chamfer_loss(point_cloud_src,point_cloud_tgt):
 	# point_cloud_src, point_cloud_src: b x n_points x 3  
 	# loss_chamfer = 
 	# implement chamfer loss from scratch
-    dist_st = torch.min(torch.sum((point_cloud_src.unsqueeze(2) - point_cloud_tgt.unsqueeze(1))**2, dim=3), dim=2)[0]
-    dist_ts = torch.min(torch.sum((point_cloud_tgt.unsqueeze(2) - point_cloud_src.unsqueeze(1))**2, dim=3), dim=2)[0]
+    dist_st = pytorch3d.ops.knn_points(point_cloud_src, point_cloud_tgt, K=1)
+    dist_ts = pytorch3d.ops.knn_points(point_cloud_tgt, point_cloud_src, K=1)
     loss_chamfer = torch.mean(dist_st) + torch.mean(dist_ts)
     return loss_chamfer
 
 def smoothness_loss(mesh_src):
 	# loss_laplacian = 
 	# implement laplacian smoothening loss
-    vertices = mesh_src.verts_packed()
-    edges = mesh_src.edges_packed()
-    v1 = vertices[edges[:, 0]]
-    v2 = vertices[edges[:, 1]]
-    laplacian = v1 - v2
-    loss_laplacian = torch.mean(torch.sum(laplacian ** 2, dim=1))
+    loss_laplacian = pytorch3d.loss.mesh_laplacian_smoothing(mesh_src)
     return loss_laplacian
